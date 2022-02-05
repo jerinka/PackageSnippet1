d82d1fc2fbe6a6cf02cca68ba3e40e5d1d882d7b
Jerin <jerin.electronics@gmail.com>
2022-02-05 17:22:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="PackageSnippet1",
-    version="0.0.3",
+    version="0.0.4",
     author="Jerin KA",
     author_email="me@example.com",
     description="Common utils package",
